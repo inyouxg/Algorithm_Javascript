3494a2afd4796e3b4bab5465418359867a5b91ce
inyouxg <dolley03@naver.com>
2026-05-13 17:24:50 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 12.41 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,13 @@
+function solution(arr) {
+    const answer = [];
+
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] !== answer[answer.length-1]) {
+            answer.push(arr[i]);
+        }
+
+        before = arr[i];
+    }
+
+    return answer;
+}
